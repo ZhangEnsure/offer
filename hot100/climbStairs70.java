d8bb0bad8843141ade6f669f97199733695b6eab
ZhangEnsure <1695313158@qq.com>
2024-11-23 20:47:05 +0800

    动态规划

---
+++ hot100/climbStairs70.java
@@ -0,0 +1,23 @@
+public class climbStairs70 {
+    public int solution(int n) {
+        // if (n == 1) {
+        //     return 1;
+        // }
+        // if (n == 2) {
+        //     return 2;
+        // }
+        // q, k, v
+        int q ,k ,v;
+        v = 0;
+        // 0 -> 0
+        q = 1;
+        // 0 -> 1
+        k = 1;
+        for(int i = 0; i < n; ++i) {
+            v = q + k;
+            q = k;
+            k = v;
+        }
+        return v;
+    }
+}
